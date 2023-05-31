cc0c8a1160aadd588166ce2140b976f6486e0cbb
INFO20005Olivia <126854036+mingchent@users.noreply.github.com>
2023-05-31 17:45:09 +1000

    started product page and the slideshow

---
+++ aprilandoak.js
@@ -0,0 +1,36 @@
+// var slides = document.querySelectorAll('.slide');
+// var currentSlide = 0;
+
+// function showSlide() {
+//    slides[currentSlide].style.opacity = 0; // Hide current slide
+//    currentSlide = (currentSlide + 1) % slides.length; // Update current slide index
+//    slides[currentSlide].style.opacity = 1; // Show new slide
+// }
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
