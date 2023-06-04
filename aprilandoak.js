df5db0534ba88857a85f1e6da88ebb11fbdb131a
INFO20005Olivia <126854036+mingchent@users.noreply.github.com>
2023-06-04 22:22:09 +1000

    finish product, cart and checkout page

--- aprilandoak.js
+++ aprilandoak.js
@@ -1,11 +1,3 @@
-// var slides = document.querySelectorAll('.slide');
-// var currentSlide = 0;
-
-// function showSlide() {
-//    slides[currentSlide].style.opacity = 0; // Hide current slide
-//    currentSlide = (currentSlide + 1) % slides.length; // Update current slide index
-//    slides[currentSlide].style.opacity = 1; // Show new slide
-// }
 
 
 let slideIndex = 1;
@@ -34,3 +26,61 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+//change image when click on different color
+function changeScr(n) {
+  if(n<1){
+    document.getElementById("Image1").src="pics/sidetable.jpeg";
+  document.getElementById("Image2").src="pics/sidetable1.jpeg";
+  document.getElementById("Image3").src="pics/sidetable2.jpeg";
+  }
+  else{
+
+  document.getElementById("Image1").src="pics/greysidetable.jpeg";
+  document.getElementById("Image2").src="pics/greysidetable1.jpeg";
+  document.getElementById("Image3").src="pics/greysidetable2.jpeg";
+
+  }
+
+}
+
+function increaseCount(a, b) {
+  var input = b.previousElementSibling;
+  var value = parseInt(input.value, 10);
+  value = isNaN(value) ? 0 : value;
+  value++;
+  input.value = value;
+}
+
+function decreaseCount(a, b) {
+  var input = b.nextElementSibling;
+  var value = parseInt(input.value, 10);
+  if (value > 1) {
+    value = isNaN(value) ? 0 : value;
+    value--;
+    input.value = value;
+  }
+}
+
+function addToCart(n){
+
+  //Assigning the variable to the user input
+  var p_quantity = document.getElementById('quantity').value;
+
+  // to print the input here
+  document.getElementById("printhere").innerHTML = p_quantity
+}
+ 
+function deleteItem(n){
+  
+  if(n<1){
+    var element= document.getElementById("product1")
+    element.remove();
+  }
+  if(n>1){
+    var element= document.getElementById("product2")
+    element.remove();
+  }
+}
+
+
